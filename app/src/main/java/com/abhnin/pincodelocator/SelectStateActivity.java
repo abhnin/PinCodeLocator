3762f4b76b0d3a98cc85a11cccd9bd5b4d08d0b7
abhishek nag <abhishek123nag@gmail.com>
2017-05-14 17:21:56 +0530

    5:21 Latest 14th May 2017

--- app/src/main/java/com/abhnin/pincodelocator/SelectStateActivity.java
+++ app/src/main/java/com/abhnin/pincodelocator/SelectStateActivity.java
@@ -1,9 +1,11 @@
 package com.abhnin.pincodelocator;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +16,19 @@ public class SelectStateActivity extends AppCompatActivity {
     private GridLayoutManager gridLayoutManager;
     private StateAdapter adapter;
     private List<StateData> data_list;
+    String stateNames[] = {"Andaman and Nicobar Islands", "Andhra Pradesh","Arunachal Pradesh","Assam", "Bihar", "Chandigarh" , "Chhattisgarh", "Dadra and Nagar Haveli" ,"Daman and Diu","Delhi",  "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Lakshyadweep", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Puducherry", "Punjab",  "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "Westbengal"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_state);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.logo);
+
+
+        actionBar.setTitle(" Select State");
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         data_list  = new ArrayList<>();
@@ -35,12 +45,17 @@ public class SelectStateActivity extends AppCompatActivity {
 
     private void load_data_from_server(int id) {
 
-        StateData data = new StateData(1, "Delhi");
-        StateData data1 = new StateData(1, "WB");
+        for (int i = 0; i < stateNames.length; i++) {
+
+            StateData data = new StateData(1, stateNames[i]);
+            data_list.add(data);
+        }
+
+        //StateData data = new StateData(1, "Delhi");
+        //StateData data1 = new StateData(2, "WB");
 
 
-        data_list.add(data);
-        data_list.add(data1);
+
 
     }
 }
